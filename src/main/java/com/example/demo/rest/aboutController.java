abc90098b0408f63d024531559d1ee70045cd7d2
xavier <lassus.xavier@gmail.com>
2021-06-20 22:55:52 +0200

    fix - init

--- src/main/java/com/example/demo/rest/aboutController.java
+++ src/main/java/com/example/demo/rest/aboutController.java
@@ -1,11 +1,13 @@
 package com.example.demo.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class aboutController {
 
+    @CrossOrigin(origins = "http://localhost:8091")
     @GetMapping("/about")
     public boolean about() {
         return true;
